87b6695d3c5f660c9822e36c2943afd10d698dfa
Matt McClure <matthewlmcclure@gmail.com>
2012-07-21 07:44:55 -0400

    Gets the Python support modules for Emacs from PyPI.

--- bootstrap-virtualenv.sh
+++ bootstrap-virtualenv.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# This assumes that you have virtualenv and virtualenvwrapper.
+# This assumes that you have virtualenv, virtualenvwrapper, and pip.
 
 . $(type -p virtualenvwrapper.sh)
-mkvirtualenv tramp-virtualenv
-workon tramp-virtualenv
-# I should really copy the local emacs*.py* to the remote python site-packages.
-cp "$(emacs --batch -q --no-site-file --eval '(message data-directory)' 2>&1)"/emacs*.py* \
-    "$(python -c 'from distutils.sysconfig import get_python_lib; print get_python_lib()')"
+workon tramp-virtualenv || mkvirtualenv tramp-virtualenv
+pip install emacs
