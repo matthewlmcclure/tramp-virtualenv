2778445ea058a54ebc988a5f3606983ad2ff195f
Matt McClure <matthewlmcclure@gmail.com>
2012-07-23 00:21:08 -0400

    Updated example .dir-locals.el per previous commit.

--- example.dir-locals.el
+++ example.dir-locals.el
@@ -2,5 +2,5 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil
-  (eval . (tve-add-to-remote-path "~/.virtualenvs/tramp-virtualenv/bin"))
+  (eval . (tve-update-remote-path "~/.virtualenvs/tramp-virtualenv/bin"))
   ))
