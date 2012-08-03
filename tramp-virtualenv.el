85d7f484db6dfdf78db6e2981d56f7f1fe68ac88
Matt McClure <matthewlmcclure@gmail.com>
2012-08-03 11:08:12 -0400

    Changed back to post-command-hook from buffer-list-update-hook. The latter sometimes runs while the current buffer is still the old buffer, and sometimes doesn't run at all when changing buffers via the buffer menu. Short circuit earlier if there's no work to be done so that interactive response doesn't suffer as much as it did when I was previously using post-command-hook.

--- tramp-virtualenv.el
+++ tramp-virtualenv.el
@@ -7,26 +7,34 @@
   (if (or dir (boundp 'tramp-virtualenv-bin-directory))
       (progn
         (and dir (set (make-local-variable 'tramp-virtualenv-bin-directory) (concat dir "/bin")))
-        (set (make-local-variable 'tramp-virtualenv-mode-line-string) (tramp-virtualenv-format-mode-line-string))
-        (if (tramp-tramp-file-p (buffer-file-name))
-            (let* ((vec (tramp-dissect-file-name (buffer-file-name)))
-                   (proc (tramp-get-connection-process vec))
-                   (new-remote-path (let ((path (tramp-get-remote-path vec)))
-                                      (setq path (delete tramp-virtualenv-bin-directory path))
-                                      (add-to-list 'path tramp-virtualenv-bin-directory)
-                                      path
-                                      )))
-              (setq tramp-remote-path (delete tramp-virtualenv-bin-directory tramp-remote-path))
-              (add-to-list 'tramp-remote-path tramp-virtualenv-bin-directory)
-              (tramp-set-connection-property vec  "remote-path" new-remote-path)
-              (tramp-set-connection-property proc "remote-path" new-remote-path)
-              (tramp-set-remote-path vec)))
-        (progn
-          (setq exec-path (delete tramp-virtualenv-bin-directory exec-path))
-          (add-to-list 'exec-path tramp-virtualenv-bin-directory)
-          (setenv "PATH" (mapconcat 'identity exec-path ":")))
-        (tramp-virtualenv-minor-mode t))
+        (if (or (not (boundp 'tramp-virtualenv-last-bin-directory))
+                (not (equal tramp-virtualenv-bin-directory tramp-virtualenv-last-bin-directory)))
+            (progn
+              (set (make-local-variable 'tramp-virtualenv-mode-line-string) (tramp-virtualenv-format-mode-line-string))
+              (if (tramp-tramp-file-p (buffer-file-name))
+                  (let* ((vec (tramp-dissect-file-name (buffer-file-name)))
+                         (proc (tramp-get-connection-process vec))
+                         (new-remote-path (let ((path (tramp-get-remote-path vec)))
+                                            (setq path (delete tramp-virtualenv-bin-directory path))
+                                            (add-to-list 'path tramp-virtualenv-bin-directory)
+                                            path
+                                            )))
+                    (message "tramp-virtualenv remote")
+                    (setq tramp-remote-path (delete tramp-virtualenv-bin-directory tramp-remote-path))
+                    (add-to-list 'tramp-remote-path tramp-virtualenv-bin-directory)
+                    (tramp-set-connection-property vec  "remote-path" new-remote-path)
+                    (tramp-set-connection-property proc "remote-path" new-remote-path)
+                    (tramp-set-remote-path vec))
+                (progn
+                  (message "tramp-virtualenv local")
+                  (setq exec-path (delete tramp-virtualenv-bin-directory exec-path))
+                  (add-to-list 'exec-path tramp-virtualenv-bin-directory)
+                  (setenv "PATH" (mapconcat 'identity exec-path ":"))))
+              (setq tramp-virtualenv-last-bin-directory tramp-virtualenv-bin-directory)
+              (tramp-virtualenv-minor-mode t))))
     (tramp-virtualenv-minor-mode -1)))
+
+(add-hook 'post-command-hook 'tramp-virtualenv)
 
 (defun tramp-virtualenv-format-mode-line-string ()
   (concat " " (nth 1 (reverse (split-string tramp-virtualenv-bin-directory "/"))))
@@ -38,7 +46,5 @@
   tramp-virtualenv-mode-line-string     ; mode line indicator
   nil                                   ; keymap
   :group 'tramp-virtualenv)             ; group
-
-(add-hook 'buffer-list-update-hook 'tramp-virtualenv)
 
 (provide 'tramp-virtualenv)
