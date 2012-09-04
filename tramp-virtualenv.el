1417d609367f33131f0ff2a212e8c62c91582d67
Matt McClure <matthewlmcclure@gmail.com>
2012-09-03 21:27:48 -0400

    Refactored to implement in terms of the virtualenv directory.
    Uses the virtualenv directory instead of the bin subdirectory. Fixes a
    bunch of edge cases. Short circuits early to improve responsiveness
    since there's a hook function in post-command-hook.

--- tramp-virtualenv.el
+++ tramp-virtualenv.el
@@ -3,53 +3,54 @@
 (if (boundp 'enable-remote-dir-locals)
     (setq enable-remote-dir-locals t))
 
-(defun tramp-virtualenv (&optional dir)
-  (if (or dir (boundp 'tramp-virtualenv-bin-directory))
-      (progn
-        (if dir
-            (progn
-              (setenv "VIRTUAL_ENV" dir)
-              ;; TODO: Save tramp-virtualenv-directory instead of
-              ;; tramp-virtualenv-bin-directory. Compute the latter.
+(defun tramp-virtualenv-bin-dir ()
+  (concat tramp-virtualenv-dir "/bin"))
 
-              ;; TODO: Set VIRTUAL_ENV in
-              ;; tramp-remote-process-environment.
-              ;; (add-to-list 'tramp-remote-process-environment "VIRTUAL_ENV=/usr/local/python/env/prod_live")
-              (message dir)
-              (set (make-local-variable 'tramp-virtualenv-bin-directory) (concat dir "/bin"))
-              (set (make-local-variable 'tramp-virtualenv-mode-line-string) (tramp-virtualenv-format-mode-line-string))
-              (tramp-virtualenv-minor-mode t)))
-        (if (or 
-             (not (boundp 'tramp-virtualenv-last-bin-directory))
-             (not (equal tramp-virtualenv-bin-directory tramp-virtualenv-last-bin-directory)))
-            (progn
-              (if (tramp-tramp-file-p (buffer-file-name))
-                  (let* ((vec (tramp-dissect-file-name (buffer-file-name)))
-                         (proc (tramp-get-connection-process vec))
-                         (new-remote-path (let ((path (tramp-get-remote-path vec)))
-                                            (setq path (delete tramp-virtualenv-bin-directory path))
-                                            (add-to-list 'path tramp-virtualenv-bin-directory)
-                                            path
-                                            )))
-                    (message "tramp-virtualenv remote")
-                    (setq tramp-remote-path (delete tramp-virtualenv-bin-directory tramp-remote-path))
-                    (add-to-list 'tramp-remote-path tramp-virtualenv-bin-directory)
-                    (tramp-set-connection-property vec  "remote-path" new-remote-path)
-                    (tramp-set-connection-property proc "remote-path" new-remote-path)
-                    (tramp-set-remote-path vec))
-                (progn
-                  (message "tramp-virtualenv local")
-                  (setq exec-path (delete tramp-virtualenv-bin-directory exec-path))
-                  (add-to-list 'exec-path tramp-virtualenv-bin-directory)
-                  (setenv "PATH" (mapconcat 'identity exec-path ":"))))
-              (setq tramp-virtualenv-last-bin-directory tramp-virtualenv-bin-directory))))
-    (tramp-virtualenv-minor-mode -1)))
+(defun tramp-virtualenv (&optional dir)
+  (progn
+    ;(message "entering tramp-virtualenv")
+    (if (and (boundp 'dir) dir)
+        (progn
+          ;(message "dir")
+          (set (make-local-variable 'tramp-virtualenv-dir) dir)))
+    (if (and (boundp 'tramp-virtualenv-dir) tramp-virtualenv-dir)
+        (progn
+          ;(message "tramp-virtualenv-dir")
+          (set (make-local-variable 'tramp-virtualenv-mode-line-string) (tramp-virtualenv-format-mode-line-string))
+          (tramp-virtualenv-minor-mode t)
+          (if (or
+               (not (boundp 'tramp-virtualenv-last-dir))
+               (not (equal tramp-virtualenv-dir tramp-virtualenv-last-dir)))
+              (progn
+                ;(message "new tramp-virtualenv-dir")
+                (if (tramp-tramp-file-p (buffer-file-name))
+                    (let* ((vec (tramp-dissect-file-name (buffer-file-name)))
+                           (proc (tramp-get-connection-process vec))
+                           (new-remote-path (let ((path (tramp-get-remote-path vec)))
+                                              (setq path (delete (tramp-virtualenv-bin-dir) path))
+                                              (add-to-list 'path (tramp-virtualenv-bin-dir))
+                                              path)))
+                      ;(message "tramp-virtualenv remote")
+                      (tramp-send-command
+                       vec (format "VIRTUAL_ENV=%s; export VIRTUAL_ENV" tramp-virtualenv-dir))
+                      (setq tramp-remote-path (delete (tramp-virtualenv-bin-dir) tramp-remote-path))
+                      (add-to-list 'tramp-remote-path (tramp-virtualenv-bin-dir))
+                      (tramp-set-connection-property vec  "remote-path" new-remote-path)
+                      (tramp-set-connection-property proc "remote-path" new-remote-path)
+                      (tramp-set-remote-path vec))
+                  (progn
+                    ;(message "tramp-virtualenv local")
+                    (setenv "VIRTUAL_ENV" tramp-virtualenv-dir)
+                    (setq exec-path (delete (tramp-virtualenv-bin-dir) exec-path))
+                    (add-to-list 'exec-path (tramp-virtualenv-bin-dir))
+                    (setenv "PATH" (mapconcat 'identity exec-path ":"))))
+                (setq tramp-virtualenv-last-dir tramp-virtualenv-dir))))
+      (tramp-virtualenv-minor-mode -1))))
 
 (add-hook 'post-command-hook 'tramp-virtualenv)
 
 (defun tramp-virtualenv-format-mode-line-string ()
-  (concat " " (nth 1 (reverse (split-string tramp-virtualenv-bin-directory "/"))))
-  )
+  (concat " " (nth 0 (reverse (split-string tramp-virtualenv-dir "/")))))
 
 (define-minor-mode tramp-virtualenv-minor-mode
   nil                                   ; use default docstring
